840e0c1f56fa7573f94a1c5bd2241691ee241640
Tharun-krishna-07 <tharunkrishnaprvt@gmail.com>
2026-06-21 05:57:04 +0530

    given number is +ve/-ve

---
+++ posinegizero.py
@@ -0,0 +1,11 @@
+# too check +vs or -ve or zero :
+
+num = float(input("Enter any number to check (+/-/0) :"))
+
+if (num > 0 ):
+    print("The given number is a positive number")
+elif (num < 0) :
+    print("The given number is a negative number")
+else:
+    print(f"the number is {num}")
+
