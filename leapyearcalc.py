72bb22e864a1120439aabbd46220aac7eac2684e
Tharun-krishna-07 <tharunkrishnaprvt@gmail.com>
2026-06-21 15:15:36 +0530

    find the leap year

---
+++ leapyearcalc.py
@@ -0,0 +1,8 @@
+# to check wheather the given year is leap year or not.
+
+year = int(input("Enter the year which you need to check : "))
+
+if year %4==0 :
+    print("the give year is leap year")
+else:
+    print("the given year is not a leap year")
