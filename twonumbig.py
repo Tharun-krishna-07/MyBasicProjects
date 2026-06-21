602e8409fdc53b2f8a3516cc1a5de0792ab0e510
Tharun-krishna-07 <tharunkrishnaprvt@gmail.com>
2026-06-21 19:10:10 +0530

    greatest between two number

---
+++ twonumbig.py
@@ -0,0 +1,13 @@
+# to check the greatest of two numbers :
+
+num1 = float(input("Enter a number : "))
+num2 = float(input("Enter another number : "))
+
+if num1>num2 :
+    print("The greatest among the the two given number is :",num1)
+elif num1<num2 :
+   print("The greatest among the the two given number is :",num2)
+elif num1 == num2 :
+    print("Its equal")
+else:
+    print("Invalid input")
